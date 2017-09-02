92a09bd4acb2a35c4b7f68a34180619ded1c65a6
Lucy Laurentino <9998329@BCS.NET.NZ>
2017-09-03 11:33:07 +1200

    4loop numInput

--- Program.cs
+++ Program.cs
@@ -9,14 +9,28 @@ namespace week4exercise
             //Start the program with Clear();
             Console.Clear();
 
-            
-            Console.WriteLine("Please enter in your first name");
+            //get a value from a userinput
+            /*Console.WriteLine("Please enter in your first name");
             var fname = Console.ReadLine();
             Console.WriteLine("Please enter in your last name");
             var lname = Console.ReadLine();
-            Console.WriteLine($"Your name is {fname} {lname}");
+            Console.WriteLine($"Your name is {fname} {lname}");*/
+            
 
+            //ask the user for a number input
+            var counter = 12;
+            var i = 0;
 
+            Console.WriteLine("Please enter in a number");
+            var number = int.Parse(Console.ReadLine());
+            for (i = 0; i < counter; i++)
+            {
+                var a = i + 1;
+                Console.WriteLine($"{number} x {a} = {number * a}");
+                Console.ReadLine();
+            }
+
+            
             //End the program with blank line and instructions
             Console.ResetColor();
             Console.WriteLine();
